051326441787bcf3f8245032476a8e1947185d2b
Agencia Grudigital <contato@grudigital.com.br>
2020-07-27 23:31:30 -0300

    @felipesergio There are no scheduling records with the code entered! Corrigi adicionando um intervalo menor

--- eng/cancel-reservation-step2.php
+++ eng/cancel-reservation-step2.php
@@ -154,7 +154,7 @@
                 <?php
                 require("../admin/connections/conn.php");
                 $pegaid = (int)$_GET['codigo'];
-                $sql = "select id, codigo, nome, sobrenome, email, date_format(data, '%d/%m/%Y') as datareserva, periodo, cadastroem FROM reservas where codigo = '$pegaid' and data != 'null' order by data desc";
+                $sql = "select id, codigo, nome, sobrenome, email, date_format(data, '%d/%m/%Y') as datareserva, periodo, cadastroem FROM reservas where codigo = '$pegaid' and data != 'null' and data < (CURRENT_DATE() + INTERVAL 1 DAY) order by data desc";
                 $result = mysqli_query($conn, $sql);
                 $num_rows = mysqli_num_rows($result);
                 if ($num_rows == 0) {
