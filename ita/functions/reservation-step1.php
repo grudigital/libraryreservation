63d43ef9e5fb1e114a8ec5f8af4081bc3f13cd99
Agencia Grudigital <contato@grudigital.com.br>
2020-07-28 00:48:12 -0300

    @felipesergio correcao da ordem de validacoes, primeiro vem a validacao de nome e codigo

--- ita/functions/reservation-step1.php
+++ ita/functions/reservation-step1.php
@@ -3,6 +3,16 @@ $codigo = $_REQUEST['codigo'];
 $sobrenome = $_REQUEST['sobrenome'];
 require("../../admin/connections/conn.php");
 
+/*Validacao de codigo / nome*/
+$validacaocodigosobrenome = "select * from usuarios where codigo = '$codigo' and sobrenome = '$sobrenome'";
+$resultcodigosobrenome = mysqli_query($conn, $validacaocodigosobrenome);
+$num_rowscodigosobrenome = mysqli_num_rows($resultcodigosobrenome);
+
+if ($num_rowscodigosobrenome == 'null') {
+    echo "<meta http-equiv='refresh' content=0;url='../reservation-exceeded-divergent.php'>";
+} else {
+    /*Validacao de codigo / nome*/
+
 $validacaoquantidade = "select * from reservas where codigo = '$codigo' and data between NOW() and DATE_ADD(NOW(), INTERVAL 8 DAY)";
 $result2 = mysqli_query($conn, $validacaoquantidade);
 $num_rows2 = mysqli_num_rows($result2);
@@ -11,15 +21,7 @@ if ($num_rows2 >= '4') {
     echo "<meta http-equiv='refresh' content=0;url='../reservation-exceeded.php?id=$codigo'>";
 } else {
 
-    /*Validacao de codigo / nome*/
-    $validacaocodigosobrenome = "select * from usuarios where codigo = '$codigo' and sobrenome = '$sobrenome'";
-    $resultcodigosobrenome = mysqli_query($conn, $validacaocodigosobrenome);
-    $num_rowscodigosobrenome = mysqli_num_rows($resultcodigosobrenome);
 
-    if ($num_rowscodigosobrenome == 'null') {
-        echo "<meta http-equiv='refresh' content=0;url='../reservation-exceeded-divergent.php'>";
-    } else {
-        /*Validacao de codigo / nome*/
 
         $sql = "INSERT INTO reservas (codigo,cadastroem) VALUES ('$_POST[codigo]',now())";
         if (!mysqli_query($conn, $sql)) {
